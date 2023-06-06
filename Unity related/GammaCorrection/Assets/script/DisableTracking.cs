ccc5a1c4730b809f7f854102ec4279ca2a6f3160
fengxuexingchen <yangguretong@gmail.com>
2023-06-06 16:21:40 +0800

    add

---
+++ Unity related/GammaCorrection/Assets/script/DisableTracking.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.XR;
+
+public class DisableTracking : MonoBehaviour
+{
+    private GameObject cam;
+    
+    void Start()
+    {
+        XRDevice.DisableAutoXRCameraTracking(GetComponent<Camera>(), true);   
+        cam = GameObject.Find("Main Camera");                               
+        cam.transform.position = new Vector3(0, 0, 0);                                    
+        cam.transform.rotation = new Quaternion(0, 0, 0, 0);                 
+    }
+    
+    void Update()
+    {
+        
+    }
+
+}
